c6cb26a782477d2b02b5867a506b42bb3614de77
Ruan <rpablooliveira2@gmail.com>
2023-10-17 11:46:23 -0300

    att

--- App.js
+++ App.js
@@ -1,20 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [peso, setPeso] = useState(''); //armazena o peso
+  const [altura, setAltura] = useState(''); //armazena o altura
+
+  function executarCalculos(){
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+    
+    if(imc < 18.6){
+      alert('Você está abaixo do peso - IMC = ' + imc.toFixed(2));
+    }else if(imc >= 18.6 && imc < 24.9){
+      alert('Você está com peso ideal - IMC = ' + imc.toFixed(2));
+    }else if(imc >= 24.9 && imc < 34.9){
+      alert('Você está levemente acima do peso - IMC = ' + imc.toFixed(2));
+    }else if(imc >= 34.9){
+      alert('Você está acima do peso - IMC = ' + imc.toFixed(2));
+    }
+    setAltura('');
+    setPeso('');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilo.container}>
+      <Text style={estilo.title}> Calcule seu IMC</Text>
+
+      <TextInput
+        style={estilo.input}
+        value={peso}                      // valor dentro do componente
+        onChangeText={setPeso}            // toda vez que o campo mudar ele é salvo
+        placeholder="Peso (kg)"
+        keyboardType="numeric"
+      />
+
+      <TextInput
+        style={estilo.input}
+        value={altura}                    // valor dentro do componente
+        onChangeText={setAltura}          // toda vez que o campo mudar ele é salvo
+        placeholder="Altura (cm)"
+        keyboardType="numeric"
+      />
+
+      <TouchableOpacity style={estilo.botao}
+        onPress={executarCalculos}>
+        <Text style={estilo.textoBotao}>Calcular</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const estilo = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  title: {
+    textAlign: 'center',
+    marginTop: 25,
+    fontSize: 30
+  },
+  input: {
+    backgroundColor: '#DDD',
+    borderRadius: 10,
+    margin: 15,
+    padding: 10,
+    color: '#000',
+    fontSize: 23
+  },
+  botao: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    backgroundColor: '#41AEF4',
+    borderRadius: 10,
+    padding: 10,
+  },
+  textoBotao: {
+    color: '#FFF',
+    fontSize: 25,
+  }
 });
